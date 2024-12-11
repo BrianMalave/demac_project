65f8422d1eebe4af4b798f86161fe0323fc8bd4d
BrianMalave <brianproject007@gmail.com>
2024-12-11 07:52:58 -0300

    update_v8

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,8 +1,17 @@
 import { useEffect } from "react";
-import { elementObserver } from '../utils/utils';
-import { civiles, industriales, maritima, mineria, viales } from '../assets/assets';
+import { elementObserver } from "../utils/utils";
+import { civiles, industriales, maritima, mineria, viales } from "../assets/assets";
 
 const Field = () => {
+  const handleImageClick = (tab) => {
+    const event = new CustomEvent("changeTab", { detail: { tab } });
+    window.dispatchEvent(event);
+
+    const partnersSection = document.querySelector("#partners");
+    if (partnersSection) {
+      partnersSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   useEffect(() => {
     const fieldImages = document.querySelectorAll(".fieldImagesSubContainer");
@@ -18,44 +27,48 @@ const Field = () => {
   }, []);
 
   return (
-    <section className='fieldSection'>
-      <div className='fieldInfoContainer'>
+    <section className="fieldSection">
+      <div className="fieldInfoContainer">
         <div className="fieldInfoTitleContainer">
-          <h1 className="fieldTitle">Destacamos en proyectos diversos, con un enfoque de calidad, seguridad y excelencia</h1>
+          <h1 className="fieldTitle">
+            Destacamos en proyectos diversos, con un enfoque de calidad, seguridad y excelencia
+          </h1>
         </div>
         <div className="fieldInfoDescription">
           <p className="fieldDescription">
-            Contamos con un equipo multidisciplinario altamente calificado, lo que nos permite gestionar simultáneamente el desarrollo y construcción de diversos proyectos de manera eficiente y eficaz.
+            Contamos con un equipo multidisciplinario altamente calificado, lo que nos permite
+            gestionar simultáneamente el desarrollo y construcción de diversos proyectos de manera
+            eficiente y eficaz.
           </p>
         </div>
       </div>
       <div className="fieldImagesContainer">
         <div className="fieldImagesSubContainer motion-down">
-          <a href="#partners">
+          <a onClick={() => handleImageClick("aero")}>
             <img src={civiles} alt="Obras Civiles por Demac" className="fieldImages" />
             <h2>Obras Civiles</h2>
           </a>
         </div>
         <div className="fieldImagesSubContainer motion-up">
-          <a href="#partners">
+          <a onClick={() => handleImageClick("industriales")}>
             <img src={industriales} alt="Obras Industriales por Demac" className="fieldImages" />
             <h2>Industriales</h2>
           </a>
         </div>
         <div className="fieldImagesSubContainer motion-up">
-          <a href="#partners">
+          <a onClick={() => handleImageClick("industriales")}>
             <img src={mineria} alt="Obras Mineras por Demac" className="fieldImages" />
             <h2>Mineras</h2>
           </a>
         </div>
         <div className="fieldImagesSubContainer motion-up">
-          <a href="#partners">
+          <a onClick={() => handleImageClick("maritimas")}>
             <img src={maritima} alt="Obras Maritimas por Demac" className="fieldImages" />
             <h2>Marítimas</h2>
           </a>
         </div>
         <div className="fieldImagesSubContainer motion-down">
-          <a href="#partners">
+          <a onClick={() => handleImageClick("viales")}>
             <img src={viales} alt="Obras Viales por Demac" className="fieldImages" />
             <h2>Viales</h2>
           </a>
--- src/components/Partner.jsx
+++ src/components/Partner.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
@@ -9,7 +9,7 @@ import {
   comsaLogo,
   asmarLogo,
   chevronDown,
-  chevronRight
+  chevronRight,
 } from "../assets/assets";
 import { viales, maritimas, aeroportuarias, industriales } from "../constants/constants";
 
@@ -22,27 +22,23 @@ const Partner = () => {
   };
 
   const handleToggle = (tab, index) => {
-    setOpenIndexes((prevState) => {
-      const newState = {
-        ...prevState,
-        [tab]: prevState[tab] === index ? null : index
-      };
-      return newState;
-    });
+    setOpenIndexes((prevState) => ({
+      ...prevState,
+      [tab]: prevState[tab] === index ? null : index,
+    }));
   };
+  useEffect(() => {
+    const handleCustomTabChange = (event) => {
+      setActiveTab(event.detail.tab);
+    };
+    window.addEventListener("changeTab", handleCustomTabChange);
 
-  const images = [
-    besalcoLogo,
-    icafalLogo,
-    ferrovialLogo,
-    comsaLogo,
-    asmarLogo
-  ];
+    return () => {
+      window.removeEventListener("changeTab", handleCustomTabChange);
+    };
+  }, []);
 
-  const swiperStyle = {
-    width: "100%",
-    height: "140px"
-  };
+  const images = [besalcoLogo, icafalLogo, ferrovialLogo, comsaLogo, asmarLogo];
 
   const renderObras = (obras, tabKey) => {
     return obras.map((obra, index) => (
@@ -55,7 +51,7 @@ const Partner = () => {
             fontWeight: "bold",
             display: "flex",
             justifyContent: "space-between",
-            alignItems: "center"
+            alignItems: "center",
           }}
         >
           {obra.title}
@@ -66,8 +62,9 @@ const Partner = () => {
           />
         </div>
         <div
-          className={`partnerProjectInfoClients ${openIndexes[tabKey] === index ? "open" : ""
-            }`}
+          className={`partnerProjectInfoClients ${
+            openIndexes[tabKey] === index ? "open" : ""
+          }`}
         >
           <div className="partnerFirstContainer">
             <div className="partnerProjectDate">Año: {obra.year}</div>
@@ -79,36 +76,6 @@ const Partner = () => {
                 {obra.description}
               </p>
             </div>
-            <div className="partnerLogoContainerAlt">
-              <h3>Clientes:</h3>
-              <div className="clientContainer">
-                {obra.clients.map((client, i) => (
-                  <img
-                    key={i}
-                    src={client.logo}
-                    alt={client.alt}
-                    className="clientLogo"
-                  />
-                ))}
-              </div>
-            </div>
-            <div className="partnerProjectContainerAlt">
-              <div className="clientProjectContainer">
-                <img
-                    src={obra.project}
-                    alt={"Imagen de proyecto"}
-                    className="clientProject"
-                  />
-                {/* {obra.project.map((p, i) => (
-                  <img
-                    key={i}
-                    src={p.project}
-                    // alt={p.alt}
-                    className="clientProject"
-                  />
-                ))} */}
-              </div>
-            </div>
           </div>
         </div>
       </div>
@@ -116,41 +83,7 @@ const Partner = () => {
   };
 
   return (
-    <section className="partnerSection">
-      <div className="partnerTitleContainer">
-        <h1 className="partnerTitle">
-          Partners que nos han elegido por nuestra experiencia
-        </h1>
-      </div>
-
-      {/* SLIDERS PARTNER */}
-      <Swiper
-        modules={[Navigation, Pagination, Autoplay]}
-        spaceBetween={0}
-        breakpoints={{
-          640: { slidesPerView: 1 },
-          1024: { slidesPerView: 5 }
-        }}
-        pagination={{ clickable: true }}
-        autoplay={{ delay: 3000, disableOnInteraction: false }}
-        loop={true}
-        speed={3000}
-        style={swiperStyle}
-      >
-        {images.map((image, index) => (
-          <SwiperSlide key={index}>
-            <div className="slide-Partner-content">
-              <img
-                className="slide-Partner-image"
-                src={image}
-                alt={`Partner Demac ${index + 1}`}
-              />
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-
-      {/* TAB BUTTONS */}
+    <section id="partners" className="partnerSection">
       <div className="tabContainer">
         <button
           className={`tabButton ${activeTab === "viales" ? "active" : ""}`}
@@ -158,21 +91,18 @@ const Partner = () => {
         >
           Obras Viales
         </button>
-        <div className="spacer"></div>
         <button
           className={`tabButton ${activeTab === "maritimas" ? "active" : ""}`}
           onClick={() => handleTabChange("maritimas")}
         >
           Obras Marítimas
         </button>
-        <div className="spacer"></div>
         <button
           className={`tabButton ${activeTab === "aero" ? "active" : ""}`}
           onClick={() => handleTabChange("aero")}
         >
           Obras Aeroportuarias
         </button>
-        <div className="spacer"></div>
         <button
           className={`tabButton ${activeTab === "industriales" ? "active" : ""}`}
           onClick={() => handleTabChange("industriales")}
@@ -181,27 +111,18 @@ const Partner = () => {
         </button>
       </div>
 
-      {/* TABS CONTENT */}
       <div className="partnerMainContainer">
-        <section
-          className={`obra-section ${activeTab === "viales" ? "active" : ""}`}
-        >
+        <section className={`obra-section ${activeTab === "viales" ? "active" : ""}`}>
           {renderObras(viales, "viales")}
         </section>
-        <section
-          className={`obra-section ${activeTab === "maritimas" ? "active" : ""
-            }`}
-        >
+        <section className={`obra-section ${activeTab === "maritimas" ? "active" : ""}`}>
           {renderObras(maritimas, "maritimas")}
         </section>
-        <section
-          className={`obra-section ${activeTab === "aero" ? "active" : ""}`}
-        >
+        <section className={`obra-section ${activeTab === "aero" ? "active" : ""}`}>
           {renderObras(aeroportuarias, "aero")}
         </section>
         <section
-          className={`obra-section ${activeTab === "industriales" ? "active" : ""
-            }`}
+          className={`obra-section ${activeTab === "industriales" ? "active" : ""}`}
         >
           {renderObras(industriales, "industriales")}
         </section>
